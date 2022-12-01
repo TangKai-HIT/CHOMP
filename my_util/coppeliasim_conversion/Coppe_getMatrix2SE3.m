eab9e67fbb50559fee91d482d8edf96cf7d67897
TangKai-HIT <1292737216@qq.com>
2022-12-02 00:12:59 +0800

    use 'cellfun' and 'cell2mat' for conversion

--- my_util/coppeliasim_conversion/Coppe_getMatrix2SE3.m
+++ my_util/coppeliasim_conversion/Coppe_getMatrix2SE3.m
@@ -3,11 +3,11 @@ function T = Coppe_getMatrix2SE3(getMatrix)
 %to SE3
 %   Detailed explanation goes here
 T = zeros(4,4);
-convMatrix = zeros(1, length(getMatrix));
+convMatrix = cell2mat(cellfun(@double, getMatrix));
 
-for i=1:length(getMatrix) %convert to double
-    convMatrix(i) = double(getMatrix{i});
-end
+% for i=1:length(getMatrix) %convert to double
+%     convMatrix(i) = double(getMatrix{i});
+% end
 
 for i=1:3
     T(i,:) = convMatrix((i-1)*4+1 : (i-1)*4+4);
