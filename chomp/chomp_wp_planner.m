babb492416eb99f5bc9f0dfd794267c7a315e3e5
TangKai-HIT <1292737216@qq.com>
2022-12-04 12:35:49 +0800

    add more utilities, improve some codes

--- chomp/chomp_wp_planner.m
+++ chomp/chomp_wp_planner.m
@@ -7,6 +7,7 @@ classdef chomp_wp_planner < handle
         constraints
 
         num_pt % number of way points to be optimized
+        total_pt % number of total way points
         dim % dimension of way points
         smooth_order % 1~3
         init_traj
@@ -22,7 +23,8 @@ classdef chomp_wp_planner < handle
     end
 
     methods
-        function obj = chomp_wp_planner(init_traj, smooth_order, isEndPtFixed)
+        %% CONSTRUCTOR
+        function obj = chomp_wp_planner(init_traj, smooth_order, isEndPtFixed, isFullSmoothMatrix)
             %CHOMP_WP_PLANNER Construct an instance of this class
             %   Detailed explanation goes here
             addpath(genpath(pwd));
@@ -33,39 +35,62 @@ classdef chomp_wp_planner < handle
             obj.init_traj = init_traj;
             obj.optim_traj = init_traj; % n X dim
             
+            obj.total_pt = size(init_traj, 1);
             obj.dim = size(init_traj, 2);
             obj.constraints = chomp_set_constraints(obj.dim);
+               
+            % record boundary points 
+            if isFullSmoothMatrix % use full smooth matrix?
+                reduceNum = smooth_order;
+            else
+                reduceNum = 1;
+            end
 
             if isEndPtFixed % fixed end point?
-                obj.num_pt = size(init_traj, 1) - 2;
-                obj.boudary_pt_Id{1} = 1;
-                obj.boudary_pt_Id{2} =  size(init_traj, 1);
+                obj.num_pt = obj.total_pt - 2*reduceNum; % exclude boudary control points (2*smooth_order)
+                obj.boudary_pt_Id{1} = 1 : reduceNum;
+                obj.boudary_pt_Id{2} =  (obj.total_pt-reduceNum+1) : obj.total_pt;
                 obj.options.FreeEndPoint = 0;
             else
-                obj.num_pt = size(init_traj, 1) - 1;
-                obj.boudary_pt_Id{1} = 1;
+                obj.num_pt = obj.total_pt - reduceNum;
+                obj.boudary_pt_Id{1} = 1 : reduceNum;
                 obj.options.FreeEndPoint = 1;
             end
 
         end
-        
+
+        %% SET_COST_GRAD_FN
         function set_cost_grad_fn(obj, costfnHandles, gradfnHandles)
             obj.cost_func = costfnHandles;
             obj.grad_func = gradfnHandles;
         end
+        
+        %% GET_BOUNDARY_PT
+        function [startpoints, endpoints] = get_boundary_pt(obj)
+            %GET_BOUNDARY_PT return boundary points for differetiation
+            startpoints = obj.init_traj(obj.boudary_pt_Id{1}, :);
+            endpoints = obj.init_traj(obj.boudary_pt_Id{2}, :);
+        end
 
-        function [optim_traj, cost_best, exitflag, output] = solve(obj)
+        %% SOLVE
+        function [optim_traj, cost_best, exitflag, output] = solve(obj, method)
             %SOLVE iteratively solve the squential quadratic programming problem
             %   Detailed explanation goes here
             if isempty(obj.constraints.C) && isempty(obj.constraints.lb) && isempty(obj.constraints.ub)
                 [optim_traj , cost_best, exitflag, output ] = chomp_CovGradDescent( obj.init_traj, obj.boudary_pt_Id, ...
                                                                                                          obj.cost_func, obj.grad_func, obj.constraints, obj.options );
+            else % with inequality constraint
+                if ~exist("method","var")
+                    method = 'qp';
+                end
+                
             end
             
             obj.solverLog = output;
             obj.optim_traj = optim_traj;
         end
-    
+        
+        %% DEFORMHISTORY_PLOT
         function deformHistory_plot(obj, ax, cmapHandle)
             %DEFORMHISTORY_PLOT plot history of 2D/3D trajectory deformation by a
             %color map
