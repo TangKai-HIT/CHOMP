d7b819af34b111f8d2ada69821514ec9d3c75184
TangKai-HIT <1292737216@qq.com>
2022-12-04 12:37:15 +0800

    support higher differetiation order

--- chomp/cost_functions/costfn_smooth_wp.m
+++ chomp/cost_functions/costfn_smooth_wp.m
@@ -1,4 +1,4 @@
-function cost = costfn_smooth_wp( xi, A, b, c , dt)
+function cost = costfn_smooth_wp( xi, A, b, c , dt, smooth_order)
 %COSTFN_SMOOTH_WP Calculate smoothness cost of a trajectory (way point parameterization)
 %   xi: discretized waypoint trajectory
 %   A: smoothness quadratic matrix
@@ -6,7 +6,8 @@ function cost = costfn_smooth_wp( xi, A, b, c , dt)
 %   c: smoothness constant matrix
 %   cost: smoothness cost
 %   dt: sample time
+%   smooth_order: order of smoothness (1~3)
 
-cost = 1/dt * trace(0.5*xi'*A*xi + xi'*b + c);
+cost = (1/dt)^(2*smooth_order - 1) * trace(0.5*xi'*A*xi + xi'*b + c);
 
 end
--- chomp/gradients/grad_costfn_smooth_wp.m
+++ chomp/gradients/grad_costfn_smooth_wp.m
@@ -1,10 +1,11 @@
-function grad = grad_costfn_smooth_wp( xi, A, b ,dt)
+function grad = grad_costfn_smooth_wp( xi, A, b ,dt, smooth_order)
 %GRAD_COSTFN_SMOOTH_WP Calculate smoothness gradient
 %   xi: discretized waypoint trajectory
 %   A: smoothness quadratic matrix
 %   b: smoothness linear matrix
 %   cost: smoothness cost
 %   dt: sample time
+%   smooth_order: order of smoothness (1~3)
 
-grad = 1/dt *(A*xi+b); 
+grad = (1/dt)^(2*smooth_order - 1) *(A*xi+b); 
 end
