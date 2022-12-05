f24099f1ae8e53edbd1812b90794652449552f98
TangKai-HIT <1292737216@qq.com>
2022-12-05 16:12:59 +0800

    init Ceq deq first

--- chomp/constraint_util/add_goal_equconstraints.m
+++ chomp/constraint_util/add_goal_equconstraints.m
@@ -4,9 +4,12 @@ function constraints = add_goal_equconstraints(constraints, pt, n)
 %   pt: cell array of dim d for equality 
 %   n: dimension of traj
 
+ if isempty(constraints.Ceq)
+        constraints.Ceq = cell(1, length(pt)); 
+        constraints.deq = cell(1, length(pt)); 
+end
+
 for dim = 1:length(pt)
-    constraints.Ceq{dim} = []; 
-    constraints.deq{dim} = [];
     if (~isempty(pt{dim}))
         constraints.Ceq{dim} = [constraints.Ceq{dim}; [zeros(1,n-1) 1]];
         constraints.deq{dim} = [constraints.deq{dim}; pt{dim}]; 
