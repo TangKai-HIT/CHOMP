6610b97d558f47f9c0c87f3315f1e3dc7b506570
TangKai-HIT <1292737216@qq.com>
2022-12-07 14:29:22 +0800

    added axis specification

--- softwares/matlab_environment_generation/map_common/visualize_cost_map.m
+++ softwares/matlab_environment_generation/map_common/visualize_cost_map.m
@@ -4,12 +4,16 @@
 % For License information please see the LICENSE file in the root directory.
 %
 %%
-function visualize_cost_map( map )
+function visualize_cost_map( map , ax)
 %VISUALIZE_MAP Visualize a cost map (where high values are expensive)
 %   map: map struct
 
 colormap(flipud(gray))
-imagesc([map.origin(1) map.origin(1)+map.resolution*size(map.table,1)], [map.origin(2) map.origin(2)+map.resolution*size(map.table,2)], map.table');
+if exist("ax", "var")
+    imagesc(ax, [map.origin(1) map.origin(1)+map.resolution*size(map.table,1)], [map.origin(2) map.origin(2)+map.resolution*size(map.table,2)], map.table');
+else
+    imagesc([map.origin(1) map.origin(1)+map.resolution*size(map.table,1)], [map.origin(2) map.origin(2)+map.resolution*size(map.table,2)], map.table');
+end
 axis xy;
 
 end
