c584d50a5230c1f447b15f9de0ab8df5f4327d68
TangKai-HIT <1292737216@qq.com>
2022-12-07 14:36:26 +0800

    added color specification

--- my_util/plots/plotSerialRobot2D.m
+++ my_util/plots/plotSerialRobot2D.m
@@ -1,17 +1,21 @@
-function plotSerialRobot2D(ax, robot, config, basePos)
+function plotSerialRobot2D(ax, robot, config, basePos, color)
 %PLOTSERIALROBOT2D 
 X = zeros(1,robot.NumBodies);
 Y = zeros(1, robot.NumBodies);
+
+if ~exist("color", "var")
+    color = 'black';
+end
 
 hold on;
 for i=1:robot.NumBodies
     trans = getTransform(robot,config,robot.BodyNames{i});
     X(i) = trans(1,4) + basePos(1);
     Y(i) = trans(2,4) + basePos(2);
-    plot(ax, X(i), Y(i), 'o', 'MarkerSize',10, 'Color', 'black', 'MarkerFaceColor','black'); 
+    plot(ax, X(i), Y(i), 'o', 'MarkerSize',10, 'Color', color, 'MarkerFaceColor', color); 
 
     if i>1
-        plot(ax, X(i-1:i), Y(i-1:i), '-k', 'MarkerSize',6);
+        plot(ax, X(i-1:i), Y(i-1:i), "LineStyle",'-',"Color",color, 'MarkerSize',6);
     end
 end
 
