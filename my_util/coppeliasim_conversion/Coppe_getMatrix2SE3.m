6eed96dd920466585089eed9a50248f929962ad0
TangKai-HIT <1292737216@qq.com>
2023-05-10 16:18:20 +0800

    using cellfun for converting cell data to mat

--- my_util/coppeliasim_conversion/Coppe_getMatrix2SE3.m
+++ my_util/coppeliasim_conversion/Coppe_getMatrix2SE3.m
@@ -3,15 +3,9 @@ function T = Coppe_getMatrix2SE3(getMatrix)
 %to SE3
 %   Detailed explanation goes here
 T = zeros(4,4);
-convMatrix = cell2mat(cellfun(@double, getMatrix));
+convMatrix = cellfun(@double, getMatrix);
 
-% for i=1:length(getMatrix) %convert to double
-%     convMatrix(i) = double(getMatrix{i});
-% end
-
-for i=1:3
-    T(i,:) = convMatrix((i-1)*4+1 : (i-1)*4+4);
-end
+T(1:3, :) = reshape(convMatrix, 3, 4);
 T(4,:) = [0,0,0,1];
 end
 
